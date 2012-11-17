f683bf7711fa8e215c99e819e80cba0064f29715
Sarandos Klikizos <sarandi@gmail.com>
2012-11-17 11:18:03 -0600

    Cleaned up naming, replaced newsize function with switch statement for clarity and speed

--- jtextsizer/jquery.jtextsizer.js
+++ jtextsizer/jquery.jtextsizer.js
@@ -1,30 +1,36 @@
-(function($){
-	$.fn.jtextsizer = function(){
+(function ($) {
+	"use strict";
+	$.fn.jtextsizer = function () {
 		var options = {
-    	sizedefault: "21px",
-    	inc: 20,
-    	cr: "\r\n"
-		};
+				sizedefault: "21px",
+				inc: 20,
+				cr: "\r\n"
+			},
+			temp = [
+				'<ul class=\"textsizer clearfix round\">',
+				'<li class=\"minus\">A</li>',
+				'<li class=\"default\">A</li>',
+				'<li class=\"plus\">A</li>',
+				'</ul>'
+			];
 
-		var temp = [
-   		"<ul class=\"textsizer clearfix round\">",
-   		"<li class=\"minus\">A</li>",
-    	"<li class=\"default\">A</li>",
-    	"<li class=\"plus\">A</li>",
-   		"</ul>"
-		];
-
-		var newsize = function(operator) {
-			var start = parseInt($("p").css("font-size").replace(/px/, ""),10);	
-			return operator==="plus" ? start+options.inc : start-options.inc;
-		};
-		
-		return this.each(function(){
+		return this.each(function () {
 			$(this).after(temp.join(options.cr));
-			$(".textsizer li").click(function() {
-				var currentli = $(this).attr("class");
-				currentli!=="default" ? $("p").css("font-size", newsize(currentli) + "px") : $("p").css({'font-size': options.sizedefault});
+			$(".textsizer li").click(function () {
+				var currentli = $(this).attr("class"),
+					startsize = parseInt($("p").css("font-size").replace(/px/, ""), 10);
+				switch (currentli) {
+				case 'plus':
+					$("p").css({fontSize: startsize + options.inc + "px"});
+					break;
+				case 'minus':
+					$("p").css({fontSize: startsize - options.inc + "px"});
+					break;
+				case 'default':
+					$("p").css({fontSize: options.sizedefault});
+					break;
+				}
 			});
 		});
 	};
-})(jQuery);
+}(jQuery));
